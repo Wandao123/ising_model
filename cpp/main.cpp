976203fd00784fa68e1baaf428a29a0702ab086d
Wandao123 <20665675+Wandao123@users.noreply.github.com>
2020-06-30 09:51:50 +0900

    実験例を変更

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -7,11 +7,11 @@
 
 int main()
 {
-    const unsigned int maxNodes = 1024;
-    const double probability = 0.5e0;
-    const unsigned int terminatedTime = 2000;
+    //const unsigned int maxNodes = 1024;
+    //const double probability = 0.5e0;
+    const unsigned int terminatedTime = static_cast<int>(1.e6);
     
-    Rand rand;
+    /*Rand rand;
     QuadraticBiases quadratic;
     for (auto i = 0; i < maxNodes; i++) {
         for (auto j = i + 1; j < maxNodes; j++) {
@@ -22,23 +22,21 @@ int main()
                 //quadratic[std::make_pair(i, j)] = +1.e0;
         }
     }
-    IsingModel isingModel({}, quadratic);
+    IsingModel isingModel({}, quadratic);*/
+    IsingModel isingModel({ {0, 1.e0}, {1, 2.e0} }, { {{0, 1}, -3.e0} });
 
     /*auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     std::stringstream ss;
     ss << std::put_time(std::localtime(&time), "%Y-&m-&d %X");
     std::cout << ss.str() << std::endl;*/
     
-    //double T0 = std::pow(maxNodes, 2);
-    double T0 = 200.e0;
-    double dT = (T0 - 1.e0) / terminatedTime;
     isingModel.ChangeAlgorithmTo(IsingModel::Algorithm::SCA);
+    isingModel.SetTemperature(1.e0);
     //isingModel.SetPinningParameter(std::sqrt(maxNodes));
-    isingModel.SetPinningParameter(22.4e0);
+    isingModel.SetPinningParameter(3.e0 / 2);
     for (auto n = 0; n <= terminatedTime; n++) {
-        isingModel.SetTemperature(T0 - n * dT);
         isingModel.Update();
-        std::cout << std::setw(4) << n
+        std::cout << std::setw(7) << std::left << n
             << std::setw(16) << std::scientific << std::setprecision(5) << isingModel.GetEnergy()
             << std::setw(16) << std::scientific << std::setprecision(7) << isingModel.GetTemperature()
             << std::endl;
