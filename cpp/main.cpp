4fb2349843a555ee40a645b00e610d968861e783
Wandao123 <20665675+Wandao123@users.noreply.github.com>
2020-07-04 13:07:49 +0900

    実験のために様々なcooling scheduleを追記

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,8 +1,10 @@
 ﻿#include "simulator.h"
 //#include <chrono>
+#include <cmath>
 //#include <ctime>
 #include <iomanip>
 #include <iostream>
+#include <numeric>
 //#include <sstream>
 
 QuadraticBiases generateErdosRenyiEdges(const int maxNodes, const double probability)
@@ -47,22 +49,29 @@ void printStatus(const IsingModel& isingModel)
 
 int main()
 {
-    //const unsigned int maxNodes = 1024;
-    //const double probability = 0.5e0;
-    const unsigned int terminatedTime = static_cast<int>(1.e3);
-
-    IsingModel isingModel({ {0, 1.e0}, {1, 2.e0} }, { {{0, 1}, -3.e0} });
+    const unsigned int maxNodes = 1024;
+    const double probability = 0.5e0;
+    const unsigned int maxTrials = static_cast<int>(1.e4);
+    auto quadratic = generateErdosRenyiEdges(maxNodes, probability);
+    IsingModel isingModel({}, quadratic);
 
     /*auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     std::stringstream ss;
     ss << std::put_time(std::localtime(&time), "%Y-&m-&d %X");
     std::cout << ss.str() << std::endl;*/
-    
-    isingModel.ChangeAlgorithmTo(IsingModel::Algorithms::SCA);
-    isingModel.SetTemperature(1.e0);
-    //isingModel.SetPinningParameter(std::sqrt(maxNodes));
-    isingModel.SetPinningParameter(3.e0 / 2);
-    for (auto n = 0; n <= terminatedTime; n++) {
+
+    isingModel.ChangeAlgorithmTo(IsingModel::Algorithms::Glauber);
+    double initialTemperature = std::accumulate(
+        quadratic.begin(), quadratic.end(), 0.e0,
+        [](double acc, const QuadraticBiases::value_type& p) -> double { return acc + std::abs(p.second); }
+    );
+    isingModel.SetTemperature(initialTemperature);
+    isingModel.SetPinningParameter(maxNodes);
+    for (auto n = 0; n <= maxTrials; n++) {
+        isingModel.SetTemperature(initialTemperature / (std::sqrt(maxNodes) * std::log(n + 1) + 1.e0));  // A logarithmic cooling schedule.
+        //isingModel.SetTemperature(initialTemperature / (n + 1.e0));  // A linear multiplicative cooling schedule.
+        //isingModel.SetTemperature(1.e0 + (initialTemperature - 1.e0) * (maxTrials - n) / maxTrials);  // A linear additive cooling schedule (whose final temperature is 1.e0).
+        //isingModel.SetTemperature(initialTemperature * std::pow(0.9e0, n));  // An exponential cooling schedule.
         isingModel.Update();
         std::cout << std::setw(7) << std::left << n
             << std::setw(16) << std::scientific << std::setprecision(5) << isingModel.GetEnergy()
