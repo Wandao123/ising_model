b45bd5b8fa4b10fb0db4cc4c5fb4cd63e1f4277a
Wandao123 <20665675+Wandao123@users.noreply.github.com>
2020-07-06 10:35:45 +0900

    実験のため修正

--- python/main.py
+++ python/main.py
@@ -5,37 +5,70 @@ __author__ = 'Wandao123'
 __date__ = '2020/6/21'
 
 import datetime
+import matplotlib.pyplot as plt
 import numpy as np
-import random
 from typing import Dict, Tuple
 
 #import simulator
 import simulatorWithCpp as simulator
 
 # Erdo"s-Re'nyi random graph.
-def generateErdosRenyiEdges(maxNodes: int) -> Dict[Tuple[int, int], float]:
-    return {(i, j): -1 if random.random() <= 0.5e0 else 0 for i in range(maxNodes) for j in range(i + 1, maxNodes)}
+def generateErdosRenyiEdges(maxNodes: int, probability: float) -> Dict[Tuple[int, int], float]:
+    return {(i, j): -1 if np.random.random() <= probability else 0 for i in range(maxNodes) for j in range(i + 1, maxNodes)}
 
 # Spin glass.
-def generateSpinGlassEdges(maxNodes: int) -> Dict[Tuple[int, int], float]:
-    return {(i, j): -1 if random.random() <= 0.5e0 else 1 for i in range(maxNodes) for j in range(i + 1, maxNodes)}
+def generateSpinGlassEdges(maxNodes: int, probability: float) -> Dict[Tuple[int, int], float]:
+    return {(i, j): -1 if np.random.random() <= probability else 1 for i in range(maxNodes) for j in range(i + 1, maxNodes)}
 
 if __name__ == '__main__':
+    # ランダム・グラフ用の乱数の初期化。
+    seed = 32
+    np.random.seed(seed=seed)
+
+    # 初期化。
+    maxTrials = int(1.e5 + 1)
     maxNodes = 1024
-    quadratic = generateErdosRenyiEdges(maxNodes)
+    probability = 0.5e0
+    quadratic = generateErdosRenyiEdges(maxNodes, probability)
     isingModel = simulator.IsingModel({}, quadratic)
-    isingModel.PinningParameter = np.sqrt(maxNodes) / 2
-    initialTemperature = np.sum([np.abs(J) + isingModel.PinningParameter for J in quadratic.values()])
+    isingModel.PinningParameter = maxNodes / 2
     isingModel.Algorithm = simulator.Algorithms.SCA
 
-    output = []
-    for i in range(int(2.e3 + 1)):
-        isingModel.Temperature = initialTemperature / (np.log(1 + i) + 1.e0)
+    # 前処理。
+    if isingModel.Algorithm == simulator.Algorithms.SCA:
         isingModel.Update()
-        output.append([i, isingModel.Energy, isingModel.Temperature])
-        if i % 20 == 0:
-            print('Complete {0} times.  Energy={1}, Temperature={2}.'.format(i, output[i][1], output[i][2]))
-    with open('output.dat', mode='w') as file:
-        file.write('# date: ' + datetime.datetime.now().isoformat() + '.\n')
-        for data in output:
-            file.write('{0:<4d} {1:<14.5e} {2:<14.7e}\n'.format(data[0], data[1], data[2]))
+        initialTemperature = 2.e0 * np.sum([np.abs(J) for J in quadratic.values()]) + maxNodes * isingModel.PinningParameter
+    else:
+        for n in range(maxNodes):
+            isingModel.Update()
+        initialTemperature = 2.e0 * np.sum([np.abs(J) for J in quadratic.values()])
+    print(np.array(list(isingModel.Spins.values())))
+
+    # サンプリング。
+    samples = np.empty((0, 3), dtype=np.float)
+    for n in range(maxTrials):
+        isingModel.Temperature = initialTemperature / (maxNodes * np.log(1 + n) + 1.e0)  # 対数スケジュール。
+        #isingModel.Temperature = initialTemperature / (n + 1.e0)  # 線形積算スケジュール。
+        #isingModel.Temperature = 1.e0 + (initialTemperature - 1.e0) * (maxTrials - n) / maxTrials  # 線形加算スケジュール。
+        #isingModel.Temperature = initialTemperature * 0.98 ** n  # 指数スケジュール。
+        isingModel.Update()
+        samples = np.append(samples, np.array([n, isingModel.Energy, isingModel.Temperature], dtype=np.float).reshape((1, 3)), axis=0)
+        if n % (maxTrials // 10) == 0:
+            print('Complete {0} times.  Energy={1}, Temperature={2}.'.format(samples[n][0], samples[n][1], samples[n][2]))
+    if samples.size > 0:
+        np.savetxt(
+            'output.dat',
+            samples,
+            fmt='%d %.5e %.7e',
+            delimiter=' ',
+            header='date: ' + datetime.datetime.now().isoformat() + '.\npinning parameter = ' + str(isingModel.PinningParameter) + '\nrandom seed = ' + str(seed)
+        )
+
+    # スコア。
+    print('The minimum energy found by the simulator: {}'.format(np.amin(samples[:, 1])))
+
+    # グラフの描画。
+    fig = plt.figure(figsize=(4, 4), dpi=200)
+    ax = fig.add_subplot(111)
+    ax.plot(samples[:, 0], samples[:, 1])
+    plt.show()
